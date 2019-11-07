05411f03b260f7ad2505fb4466f0fcce5b0cc310
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-11-07 19:19:33 +0100

    Add simple way to create random entries in db

--- app/src/main/java/test/mug/espresso/repository/PowerMugRepository.kt
+++ app/src/main/java/test/mug/espresso/repository/PowerMugRepository.kt
@@ -4,19 +4,33 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import test.mug.espresso.database.DbPowerMug
 import test.mug.espresso.database.PowerMugDatabase
 import test.mug.espresso.database.asDomainModel
 import test.mug.espresso.domain.PowerMug
-import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 
-class PowerMugRepository(database: PowerMugDatabase) {
+class PowerMugRepository(private val database: PowerMugDatabase) {
 	val powerMugs: LiveData<List<PowerMug>> = Transformations.map(database.powerMugDatabaseDao.getAll()) {
 		it.asDomainModel()
 	}
 
 	suspend fun refreshCache() {
 		withContext(Dispatchers.IO) {
-			Timber.i("Db refreshed")
+			createRandomEntryNearWroclaw()
 		}
+	}
+
+	fun createRandomEntryNearWroclaw() {
+		val x = DbPowerMug(
+			0,
+			"Test " + SimpleDateFormat("dd/M/yyyy hh:mm:ss").toString(),
+			51 + Random().nextDouble(),
+			17 + Random().nextDouble(),
+			"Testtset",
+			Random().nextInt()
+		)
+		database.powerMugDatabaseDao.insert(x)
 	}
 }
