abd979c64d91d06085d7e58505fcc021cf829a11
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-11-14 20:59:51 +0100

    Make list view prettier

--- app/src/main/java/test/mug/espresso/domain/PowerMugWithDistance.kt
+++ app/src/main/java/test/mug/espresso/domain/PowerMugWithDistance.kt
@@ -9,4 +9,18 @@ data class PowerMugWithDistance (
 	var address: String,
 	var distance: Double,
 	var numberOfMugs: Int
-)
+) {
+	fun formatDistance(distance: Double) : String {
+		var distance = distance // kotlin made all function parameters as val and they cannot be reassigned
+
+		if (distance < 0) {
+			distance *= -1
+		}
+
+		if (distance < 1) {
+			return String.format("%.0f", distance*1000) + "m"
+		} else {
+			return String.format("%.2f", distance) + "km"
+		}
+	}
+}
